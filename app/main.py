495aede4ebed2f876f7f8f34406eba6ead75e3c4
Eazdan Mostafa Rafin <eazdan.rafin@gmail.com>
2026-01-03 23:05:00 +0600

    updating users router

--- app/main.py
+++ app/main.py
@@ -5,14 +5,14 @@ from psycopg import Connection
 from psycopg.errors import UniqueViolation
 import schemas
 from typing import List
-from routers import reports
+from routers import reports, users
 
 
 app = FastAPI()
 
 
 app.include_router(reports.router)
-
+app.include_router(users.router)
 
 @app.get("/")
 async def root():
@@ -20,31 +20,6 @@ async def root():
     return{"message":"Website is UP!"}
 
 
-
-
-# -------------------USERS---------------------
-@app.get('/users')
-def get_users(db: Connection = Depends(get_db)):
-    with db.cursor() as cursor:
-        cursor.execute(''' SELECT * from users ''')
-        users = cursor.fetchall()
-    return users
-
-@app.post('/users', status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
-def post_users( user:schemas.UserCreate, db: Connection = Depends(get_db)):
-     # Hashing the Password
-     hashed_pass = utils.hash(user.password)
-     user.password = hashed_pass
-     try:
-        with db.cursor() as cursor:
-            cursor.execute('''Insert into users (email, hashed_password) VALUES (%s, %s) RETURNING id, email, created_at;''', (user.email, hashed_pass))
-            new_user = cursor.fetchone()
-        db.commit()
-        return new_user
-
-     except UniqueViolation:
-        db.rollback()
-        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Email Already Registered. Try login!")
      
         
 
---
+++ app/routers/users.py
@@ -0,0 +1,40 @@
+import utils
+from database import get_db
+from fastapi import FastAPI, Depends, HTTPException, status, Response, APIRouter
+from psycopg import Connection
+from psycopg.errors import UniqueViolation
+import schemas
+from typing import List
+
+
+router = APIRouter(
+    prefix='/users',
+    tags=['Users']
+)
+
+
+# -------------------USERS---------------------
+@router.get('/')
+def get_users(db: Connection = Depends(get_db)):
+    with db.cursor() as cursor:
+        cursor.execute(''' SELECT * from users; ''')
+        users = cursor.fetchall()
+    return users
+
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
+def post_users( user:schemas.UserCreate, db: Connection = Depends(get_db)):
+     # Hashing the Password
+     hashed_pass = utils.hash(user.password)
+     user.password = hashed_pass
+     try:
+        with db.cursor() as cursor:
+            cursor.execute('''Insert into users (email, hashed_password) VALUES (%s, %s) RETURNING id, email, created_at;''', (user.email, hashed_pass))
+            new_user = cursor.fetchone()
+        db.commit()
+        return new_user
+
+     except UniqueViolation:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Email Already Registered. Try login!")
+     
+        
